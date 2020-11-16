a0887d4f3b3e0f5aedb1a9bf98a48f9829772aaf
MadelineHenry <madelinehenry04101996@gmail.com>
2020-11-16 13:41:15 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,37 @@
+const signs = [
+    ['(',")","%","AC"],
+    ["7","8","9","/"],
+    ["4","5","6","*"],
+    ["1","2","3","-"],
+    ["0",".","=","+"]
+]
+const body = document.querySelector("body");
+const newMain = document.createElement("main");
+newMain.className = "container";
+
+const sectionDiv = document.createElement("div")
+sectionDiv.className = "section";
+
+const culumnsDiv = document.createElement("div")
+columnsDiv.className = "columns is-centered";
+
+const cardDiv = document.createElement("div")
+cardDiv.className = "card column is-one-fifth";
+
+const inputSection = document.createElement("section")
+inputSection.className = "control";
+
+const inputArea = document.createElement("input")
+inputArea.className = "input has-text-right";
+inputArea.type = "text";
+inputArea.readOnly = true;
+inputArea.placeholder = "0";
+inputArea.style.fontSize = "2em";
+inputArea.style.fontWeight = "700";
+
+control.appendChild = (input);
+
+const sectionButtons = document.createElement("section");
+sectionButtons.className = "is-centered";
+
+
